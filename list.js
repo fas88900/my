15178eada0ec1605eb30e766605fb1fa38d43d18
fas88900 <fas007714@gmail.com>
2025-02-01 00:42:06 +0700

    Update list.js

--- list.js
+++ list.js
@@ -1,114 +1,114 @@
 const videos = [
  {
     title: "Dr. Stone: Science Future",
-    image: "https://upload.wikimedia.org/wikipedia/en/e/e7/Dr_Stone_Science_Future_KV.png",
+    image: "https://fas88900.github.io/my/img/Dr_StoneScienceFuture.png",
     episodes: [
       { title: "Episode 1", url: "https://www.terabox.com/sharing/embed?surl=wZrpkmrXmKmlKnw0j-OlyA&autoplay=true&mute=false&resolution=1080" },
       { title: "Episode 2", url: "https://www.terabox.com/sharing/embed?surl=Dpwz7lv8OF4mihTmP8FdwA&autoplay=true&mute=false&resolution=1080" },
       { title: "Episode 3", url: "https://terabox.com/sharing/embed?surl=CFmENgLUqtTj1iBgn5D3pA&resolution=1080&autoplay=true&mute=false&uk=4398947362547&fid=206069812938996&slid=" },
-      { title: "Episode 4", url: "https://terabox.com/sharing/embed?surl=-4W_4eacLhFP2QSLELgYfw&resolution=1080&autoplay=true&mute=false&uk=4398947362547&fid=394075081935960&slid=" },
-     ],
+      { title: "Episode 4", url: "https://terabox.com/sharing/embed?surl=-4W_4eacLhFP2QSLELgYfw&resolution=1080&autoplay=true&mute=false&uk=4398947362547&fid=394075081935960&slid=" }
+    ],
     isSeries: true
   },
   {
     title: "Back in Action (2025)",
-    image: "https://media.themoviedb.org/t/p/w440_and_h660_face/3L3l6LsiLGHkTG4RFB2aBA6BttB.jpg",
+    image: "https://fas88900.github.io/my/img/backinaction.jpg",
     url: "https://terabox.com/sharing/embed?surl=nsj1FuHYxXdWthFqNTsTXg&resolution=1080&autoplay=true&mute=false&uk=4398947362547&fid=799361252873936&slid=",
     isSeries: false
   },
   {
     title: "Sonic the Hedgehog 3 (2024)",
-    image: "https://image.tmdb.org/t/p/original/d8Ryb8AunYAuycVKDp5HpdWPKgC.jpg",
+    image: "https://fas88900.github.io/my/img/sonic3.jpg",
     url: "https://terabox.com/sharing/embed?surl=HNgOGf0Ncdbe6fDMlsKK1A&resolution=1280&autoplay=true&mute=false&uk=4398947362547&fid=16411878115907&slid=",
     isSeries: false
   },
   {
     title: "gladiator 2",
-    image: "https://deadline.com/wp-content/uploads/2024/09/G2_DOM_Online_Payoff_1-Sheet_06_FIN12.jpg?w=800",
+    image: "https://fas88900.github.io/my/img/gladiator2.jpg",
     url: "https://www.terabox.com/sharing/embed?surl=cJVb1WeF-TLLHDfs5dutBA&autoplay=true&mute=false&resolution=1080",
     isSeries: false
   },
   {
     title: "joker folie a deux",
-    image: "https://postercinema.eu/cdn/shop/files/joker-folie-a-deux_hama8zsn.jpg?v=1726602729&width=600",
+    image: "https://fas88900.github.io/my/img/jokerfolieadeux.jpg",
     url: "https://www.terabox.com/sharing/embed?surl=OP2XcO34kAoWhjBWBL2rJw&autoplay=true&mute=false&resolution=1080",
     isSeries: false
   },
   {
     title: "Kingdom Of The Planet Of The Apes 2024",
-    image: "https://i.pinimg.com/736x/5e/af/65/5eaf652369b8219a111bd33b3cbbc1a1.jpg",
+    image: "https://fas88900.github.io/my/img/apes.jpg",
     url: "https://www.terabox.com/sharing/embed?surl=k_BKkxdHcrkxtPBG9KsmQw&autoplay=true&mute=false&resolution=1080",
     isSeries: false
   },
   {
-    title: "Panda Plan (2024)",
-    image: "https://media.themoviedb.org/t/p/w440_and_h660_face/hbO8MVrDuT8YxqeJD6DMxcamY9N.jpg",
+    title: "panda plan",
+    image: "https://fas88900.github.io/my/img/pandaplan.jpg",
     url: "https://www.terabox.com/sharing/embed?surl=fztwzPZsV91H7nZYBHdD6w&autoplay=true&mute=false&resolution=1080",
     isSeries: false
   },
   {
     title: "Transformers One (2024)",
-    image: "https://media.themoviedb.org/t/p/w440_and_h660_face/qbkAqmmEIZfrCO8ZQAuIuVMlWoV.jpg",
+    image: "https://fas88900.github.io/my/img/Transformersone.jpg",
     url: "https://terabox.com/sharing/embed?surl=S6xAeZ5WS0sAuKDAOCXkQQ&resolution=720&autoplay=true&mute=false&uk=4398947362547&fid=434830080172625&slid=",
     isSeries: false
   },
   {
     title: "Bad Boys: Ride or Die (2024)",
-    image: "https://media.themoviedb.org/t/p/w440_and_h660_face/qLyzyTXMFkqhzQ4aEeYYUoM4o18.jpg",
+    image: "https://fas88900.github.io/my/img/badboy.jpg",
     url: "https://terabox.com/sharing/embed?surl=mxtNGUjwCz5gEoFHmGTTuQ&resolution=1080&autoplay=true&mute=false&uk=4398947362547&fid=771322420005413&slid=",
     isSeries: false
   },
   {
     title: "kang mak",
-    image: "https://upload.wikimedia.org/wikipedia/id/c/ce/Poster_Kang_Mak.jpg",
+    image: "https://fas88900.github.io/my/img/Poster_Kang_Mak.jpg",
     url: "https://www.terabox.com/sharing/embed?surl=xq3FT9aKD6nsrxFeot1ECw&autoplay=true&mute=false&resolution=1080",
     isSeries: false
   },
   {
     title: "13 Bom di jakarta",
-    image: "https://upload.wikimedia.org/wikipedia/id/2/27/Poster_13_Bom_di_Jakarta.jpg",
+    image: "https://fas88900.github.io/my/img/Poster_13_Bom_di_Jakarta.jpg",
     url: "https://www.terabox.com/sharing/embed?surl=bPhJk3ch1P6tiMDOY8KHLw&autoplay=true&mute=false&resolution=1080",
     isSeries: false
   },
     {
     title: "Mufasa: The Lion King",
-    image: "https://upload.wikimedia.org/wikipedia/en/0/0b/Mufasa_The_Lion_King_Movie_2024.jpeg",
+    image: "https://fas88900.github.io/my/img/Mufasa_The_Lion_King_Movie_2024.jpeg",
     url: "https://terabox.com/sharing/embed?surl=HBiOMelXlvpkX0VhqQYowg&resolution=1080&autoplay=true&mute=false&uk=4398947362547&fid=875062506178587&slid=",
     isSeries: false
   },
     {
     title: "Kaka Boss",
-    image: "https://upload.wikimedia.org/wikipedia/id/5/50/Poster_Kaka_Boss_(2024).jpg",
+    image: "https://fas88900.github.io/my/img/Poster_Kaka_Boss_(2024).jpg",
     url: "https://terabox.com/sharing/embed?surl=X6C7wrpdaNF7kOUKfP07fQ&resolution=1080&autoplay=true&mute=false&uk=4398947362547&fid=875062506178587&slid=",
     isSeries: false
   },
    {
     title: "Venom: The Last Dance",
-    image: "https://upload.wikimedia.org/wikipedia/en/a/a3/Venom_The_Last_Dance_Poster.jpg",
+    image: "https://fas88900.github.io/my/img/Venom_The_Last_Dance_Poster.jpg",
     url: "https://terabox.com/sharing/embed?surl=iKr7FmF-Z_x9CSlvkrU_Og&resolution=1080&autoplay=true&mute=false&uk=4398947362547&fid=167472974358361&slid=",
     isSeries: false
   },
   {
     title: "Despicable Me 4",
-    image: "https://upload.wikimedia.org/wikipedia/id/e/ed/Despicable_Me_4_Theatrical_Release_Poster.jpeg",
+    image: "https://fas88900.github.io/my/img/Despicable_Me_4_Theatrical_Release_Poster.jpeg",
     url: "https://terabox.com/sharing/embed?surl=8ZIKsRMKfgqGNSmnT2DVng&resolution=1080&autoplay=true&mute=false&uk=4398947362547&fid=385334711811864&slid=",
     isSeries: false
   },
   {
     title: "Deadpool & Wolverine",
-    image: "https://media.themoviedb.org/t/p/w440_and_h660_face/ajnzOECvXpa7VcVx0RSlq39XgHe.jpg",
+    image: "https://fas88900.github.io/my/img/DeadpoolWolverine.jpg",
     url: "https://terabox.com/sharing/embed?surl=f_N_6-2U3PzJNpJXLTnnuQ&resolution=1080&autoplay=true&mute=false&uk=4398947362547&fid=853488090147033&slid=",
     isSeries: false
   },
   {
     title: "moana 2",
-    image: "https://media.themoviedb.org/t/p/w440_and_h660_face/aLVkiINlIeCkcZIzb7XHzPYgO6L.jpg",
+    image: "https://fas88900.github.io/my/img/moana2.jpg",
     url: "https://terabox.com/sharing/embed?surl=NYoTNZ-niJJcALo8HjQbbg&resolution=720&autoplay=true&mute=false&uk=4398947362547&fid=635769177309219&slid=",
     isSeries: false
   },
   {
     title: "inside out 2",
-    image: "https://media.themoviedb.org/t/p/w440_and_h660_face/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
+    image: "https://fas88900.github.io/my/img/insideout2.jpg",
     url: "https://terabox.com/sharing/embed?surl=Nl1K9NkKVkU9Cvaw-FlEoQ&resolution=720&autoplay=true&mute=false&uk=4398947362547&fid=74584052022318&slid=",
     isSeries: false
   }
